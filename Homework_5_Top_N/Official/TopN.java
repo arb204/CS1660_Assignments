feaa47fe5ccdf66b2c635bc77820628ba77356cf
arb204 <78120215+arb204@users.noreply.github.com>
2021-12-02 22:01:58 -0500

    Updated my implementation. What should be graded is in Official folder

---
+++ Homework_5_Top_N/Official/TopN.java
@@ -0,0 +1,154 @@
+import java.io.IOException;
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class TopN {
+	
+	
+	public static class TopMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+
+	    private static List<String> stopList = Arrays.asList("one", "said", "thou", "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now");
+	    private static Map<String, Integer> map;
+	    private static PriorityQueue<Entry<String, Integer>> pq;
+	    
+	    @Override
+	    public void setup(Context context) throws IOException, InterruptedException {
+	    	map = new HashMap<String, Integer>();
+	    	pq = new PriorityQueue<Map.Entry<String, Integer>>(new Comparator<Map.Entry<String, Integer>>(){
+	    		@Override
+	    		public int compare(Map.Entry<String, Integer> a, Map.Entry<String, Integer> b) {
+	    			if(a.getValue() == b.getValue()) {
+	    				return a.getKey().compareTo(b.getKey());
+	    			} else {
+	    				return a.getValue() - b.getValue();
+	    			}
+	    		}
+	    	});
+	    }
+	    
+	    @Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+	    	
+	    	String line = value.toString().replaceAll("[^a-zA-Z0-9\\s]", "").toLowerCase();
+	    	
+	    	StringTokenizer tokenizer = new StringTokenizer(line);
+	    	
+	    	while(tokenizer.hasMoreTokens()) {
+	    		String token = tokenizer.nextToken();
+	    		if(stopList.contains(token)) {
+	    			continue;
+	    		}
+	    		if(!map.containsKey(token)) {
+	    			map.put(token, 1);
+	    		} else {
+	    			Integer temp = map.get(token) + 1;
+	    			map.replace(token, temp);
+	    		}
+	    	}
+	    	
+	    }
+	    	
+	    @Override
+	    public void cleanup(Context context) throws IOException, InterruptedException {
+	    	
+	    	Iterator<Map.Entry<String, Integer>> mapIterator = map.entrySet().iterator();
+	    	
+	    	while(mapIterator.hasNext()) {
+	    		pq.add(mapIterator.next());
+	    		if(pq.size() > 5) {
+	    			pq.remove();
+	    		}
+	    	}
+	    	
+	    	while(!pq.isEmpty()) {
+	    		Map.Entry<String, Integer> entry = pq.remove();
+	    		context.write(new Text(entry.getKey()), new IntWritable(entry.getValue()));
+	    	}
+		}
+	}
+
+  public static class TopReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+	  
+	  private static PriorityQueue<Map.Entry<String, Integer>> pq;
+	  private static Map<String, Integer> map;
+    
+	  @Override
+	  public void setup(Context context) throws IOException, InterruptedException {
+		  map = new HashMap<String, Integer>();
+		  pq = new PriorityQueue<Map.Entry<String, Integer>>(new Comparator<Map.Entry<String, Integer>>(){
+	    		@Override
+	    		public int compare(Map.Entry<String, Integer> a, Map.Entry<String, Integer> b) {
+	    			if(a.getValue() == b.getValue()) {
+	    				return a.getKey().compareTo(b.getKey());
+	    			} else {
+	    				return a.getValue() - b.getValue();
+	    			}
+	    		}
+	    	});
+	  }
+	  
+	  @Override
+	  public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+		  Integer sum = 0;
+		  for(IntWritable val: values) {
+			  sum += val.get();
+		  }
+		  
+		  map.put(key.toString(), sum);
+	  }
+	  
+	  @Override
+	  public void cleanup(Context context) throws IOException, InterruptedException {
+		  Iterator<Map.Entry<String, Integer>> mapIterator = map.entrySet().iterator();
+	    	
+	    	while(mapIterator.hasNext()) {
+	    		pq.add(mapIterator.next());
+	    		if(pq.size() > 5) {
+	    			pq.remove();
+	    		}
+	    	}
+		  
+		  while(!pq.isEmpty()) {
+	    		Map.Entry<String, Integer> entry = pq.remove();
+	    		context.write(new Text(entry.getKey()), new IntWritable(entry.getValue()));
+		  }
+	  }
+  }
+
+  public static void main(String[] args) throws Exception {
+	  if(args.length != 2) {
+			 System.err.println("Usage: Top N <input path> <output path>");
+			 System.exit(0);
+		}
+	    Configuration conf = new Configuration();
+	    Job job = Job.getInstance(conf, "Top N");
+	    job.setJarByClass(TopN.class);
+	    job.setMapperClass(TopMapper.class);
+	    job.setReducerClass(TopReducer.class);
+	    job.setNumReduceTasks(1);
+	    job.setOutputKeyClass(Text.class);
+	    job.setOutputValueClass(IntWritable.class);
+	    FileInputFormat.addInputPath(job, new Path(args[0]));
+	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+	    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
---
+++ Homework_5_Top_N/readme.txt.txt
@@ -0,0 +1,3 @@
+Official now holds the official implementation to be submitted.
+Old attempt is my previous attempt that I reasoned didn't work correctly,
+after discussing the problem with another classmate.
